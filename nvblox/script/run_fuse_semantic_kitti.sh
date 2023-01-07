71556d92c32d45fc2bf28a109c32fcb97576739d
gogojjh <jiaojh1994@gmail.com>
2023-01-07 04:14:01 +0100

    update README with results on the semanticKITTI and semanticUSL dataset

--- nvblox/script/run_fuse_semantic_kitti.sh
+++ nvblox/script/run_fuse_semantic_kitti.sh
@@ -2,11 +2,11 @@
 cd build;
 make && \
 ./executables/fuse_kitti \
-	/Spy/dataset/mapping_results/nvblox/semantickitti_sequence07_ground/ \
+	/Spy/dataset/mapping_results/nvblox/semantickitti_sequence07/ \
 	--tsdf_integrator_max_integration_distance_m 100.0 \
 	--tsdf_integrator_truncation_distance_vox 5.0 \
 	--semantic_integrator_max_integration_distance_m 100.0 \
-	--num_frames 100 \
+	--num_frames 1000 \
 	--voxel_size 0.25 \
 	--mesh_integrator_min_weight 0.5 \
 	--tsdf_frame_subsampling 1 \
@@ -14,7 +14,7 @@ make && \
 	--esdf_frame_subsampling 10 \
 	--semantic_frame_subsampling 1 \
 	--mesh_output_path \
-	/Spy/dataset/mapping_results/nvblox/semantickitti_sequence07_ground_mesh_test.ply \
+	/Spy/dataset/mapping_results/nvblox/semantickitti_sequence07_mesh_1000.ply \
 	# --esdf_output_path \
 	# /Spy/dataset/mapping_results/nvblox/semantickitti_sequence07_nonground_esdf_1000.ply \
 	# --obstacle_output_path \
